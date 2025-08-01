4b592b2d44726e7c68174c99ca545abe5c688d27
abhishekrithik <abhishekrithik2017@gmail.com>
2025-08-01 13:08:17 +0530

    Made Dashboard Private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signin from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -17,11 +18,14 @@ export default function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/sign-in" element={<Signin/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
+        <Route element={<PrivateRoute/>}>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Route>
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/projects" element={<Projects/>}/>
 
     </Routes>
        <Footer/>
     </BrowserRouter>
-)
+);
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css';
 import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'; 
+import { PersistGate } from 'redux-persist/integration/react';
 import ThemeProvider from './components/ThemeProvider.jsx';
 
-createRoot(document.getElementById('root')).render(
-  
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </PersistGate>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
-
+  </PersistGate>
 );
