406184a6b7ad8b52e55141d688a99fa9e0d4c29a
abhishekrithik <abhishekrithik2017@gmail.com>
2025-08-11 23:33:46 +0530

    fixed all bugs

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -50,7 +50,7 @@ export default function SignIn() {
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
               Abhishek's
             </span>
-            Blog Sai
+            Blog 
           </Link>
           <p className='text-sm mt-5'>
             This is My Blog Project. You can Sign in with your email and password or with Google.
