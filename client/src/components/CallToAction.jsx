dbb266d06dc494f7c505d8efccb545de4e3fba09
abhishekrithik <abhishekrithik2017@gmail.com>
2025-09-14 17:43:17 +0530

    final fixes

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,10 +5,10 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Want to know more about me?
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with my JavaScript Projects
+                Checkout these resources with my other Full Stack Projects
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
                 <a href="https://github.com/abhishekrithik" target='_blank' rel='noopener noreferrer'>
